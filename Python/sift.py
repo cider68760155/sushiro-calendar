f8a9ac304fbe5ab6e9450a76c9046f67ea9dcd95
cider68760155 <asq458@outlook.jp>
2019-02-08 23:02:07 +0900

    ver3.0 Python版単純追加を実装

--- Python/sift.py
+++ Python/sift.py
@@ -3,77 +3,102 @@ import datetime
 from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
+
 import json
 from pytz import timezone
 from datetime import timedelta
+
+from selenium import webdriver
+import chromedriver_binary
+
+import re
+
+from urls import URL1,URL2
+
 # If modifying these scopes, delete the file token.json.
 #SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
 SCOPES = 'https://www.googleapis.com/auth/calendar'
 
 
 def main():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
     store = file.Storage('token.json')
     creds = store.get()
     if not creds or creds.invalid:
         flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
         creds = tools.run_flow(flow, store)
     service = build('calendar', 'v3', http=creds.authorize(Http()))
-
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                          maxResults=10, singleEvents=True,
-                                          orderBy='startTime').execute()
-    events = events_result.get('items', [])
-
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
-
-    det_add(service)
+    for event in det_add():
+        add_event(service,event)
+    
 
 
-def det_add(service):
-    event = json.loads('{\
-        "summary": "サンプルの予定",\
-        "description": "Made by userscript",\
-        "reminders": {\
-            "useDefault": false\
-        },\
-        "start": {\
-            "dateTime": ""\
-        },\
-        "end": {\
-            "dateTime": ""\
-        }\
-    }')
-    event["start"]["dateTime"] = set_timezone(2019,2,5,0,0).isoformat()
-    event["end"]["dateTime"] = set_timezone(2019,2,5,0,0).isoformat()
-    #タイムゾーン指定必要か確認！
-    add_event(service,event)
+
+def det_add():
+    events = []
+    year_sel=0
+    driver = webdriver.Chrome()
+    driver.get(URL1)
+    year = re.findall(r'[0-9]{4}', driver.find_element_by_class_name('clr').text)
+    sifts = get_sift(driver, URL1)
+    sifts.extend(get_sift(driver, URL2))
+    for sift in sifts:
+        if (sift[2] != '') & (sift[2] != '未作成'):
+            event = json.loads('{\
+                "summary": "バイト(Python版)",\
+                "description": "Made by python script",\
+                "reminders": {\
+                    "useDefault": false\
+                },\
+                "start": {\
+                    "dateTime": ""\
+                },\
+                "end": {\
+                    "dateTime": ""\
+                }\
+            }')
+            monthDay = sift[0].split('/')
+            #年末用処理
+            if (year[0]!=year[1])&(monthDay[0]=='1'):
+                year_sel=1
+            start = hm(sift[2])
+            end = hm(sift[3])
+            event["start"]["dateTime"] = set_timezone(year[year_sel], monthDay[0], monthDay[1], start[0], start[1])
+            event["end"]["dateTime"] = set_timezone(year[year_sel], monthDay[0], monthDay[1], end[0], end[1])
+            events.append(event)
+    driver.close()
+    return events
 
 
 def set_timezone(year, month, day, hour, minute):
     tz_tokyo = timezone("Asia/Tokyo")
     # naiveのdatetime作成
-    datetime_naive = datetime.datetime(year, month, day, hour, minute)
+    datetime_naive = datetime.datetime(int(year), int(month), int(day), int(hour), int(minute))
 
     # datetime(naive) -> datetime(Asia/Tokyo)
     datetime_tokyo = tz_tokyo.localize(datetime_naive)
-    return datetime_tokyo
+    return datetime_tokyo.isoformat()
 
-def add_event(service,event):
-    result=service.events().insert(calendarId='primary', body=event).execute()
-    
+
+def add_event(service, event):
+    result = service.events().insert(calendarId='primary', body=event).execute()
+    return result
+
+
+def get_sift(driver, url):
+    ret = []
+    driver.get(url)
+    sift_table = driver.find_elements_by_class_name("def")[5]
+    for row in sift_table.find_elements_by_tag_name("tr")[1:]:
+        ret.append(row.text.split(' ')[:4])
+    return ret
+
+
+def hm(time):
+    if len(time) == 3:
+        return [time[0], time[1:3]]
+    else:
+        return [time[0:2], time[2:4]]
+
 
 if __name__ == '__main__':
     main()
